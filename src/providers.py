e66f7be4f757b4ce23bd0e028b51bc9c16d57ae5
ggit12 <115842122+ggit12@users.noreply.github.com>
2025-03-28 13:15:54 -0700

    commented out anthropic for testing

--- src/providers.py
+++ src/providers.py
@@ -85,11 +85,11 @@ ENDPOINTS = ProviderEndpoints({
         # "o3-mini-2025-01-31",
         ],
     "anthropic": [
-        "claude-3-7-sonnet-20250219",
-        "claude-3-5-sonnet-20240620",
-        "claude-3-5-haiku-20241022",
-        "claude-3-opus-20240229",
-        "claude-3-haiku-20240307",
+        # "claude-3-7-sonnet-20250219",
+        # "claude-3-5-sonnet-20240620",
+        # "claude-3-5-haiku-20241022",
+        # "claude-3-opus-20240229",
+        # "claude-3-haiku-20240307",
     ],
 })
 
