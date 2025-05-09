93906655e2b8813b9426174b3fd59b4c38e21932
ggit12 <115842122+ggit12@users.noreply.github.com>
2025-05-09 11:26:44 -0700

    updated src for providers

--- src/providers.py
+++ src/providers.py
@@ -88,7 +88,7 @@ ENDPOINTS = ProviderEndpoints({
     "anthropic": [
         # "claude-3-7-sonnet-20250219",
         "claude-3-5-sonnet-20240620",
-        "claude-3-5-haiku-20241022",
+        # "claude-3-5-haiku-20241022",
         "claude-3-opus-20240229",
         "claude-3-haiku-20240307",
     ],
